4a758276683ec44d87727f738d9b545d639a8bd6
tanishp1 <pawartanish73@gmail.com>
2026-07-07 13:55:41 +0530

    components done

---
+++ 02-Component/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="Card">
+      <h1>Tanish pawar</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi sapiente
+        dolor eligendi cum ratione! Architecto id facilis, non tempore velit
+        cumque accusamus, optio perspiciatis reiciendis totam quia repellendus
+        voluptatem ipsum.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
