c6a7cb2c02de38d40ee464e4e3a66cd083fd7357
tanishp1 <pawartanish73@gmail.com>
2026-07-14 22:34:17 +0530

    done two binding

--- 05-form handling/05-form handling/src/App.jsx
+++ 05-form handling/05-form handling/src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("");
 
   const submithandler = (e) => {
-    e.preventDefault()
-    console.log('form submit')
-  }
+    e.preventDefault();
+    console.log("form submit");
+    
+    setTitle('')
+  };
   return (
     <div>
-      <form onSubmit={(e) => {
-        submithandler(e)
-      }}>
-        <input type="text" placeholder='Enter your name' />
+      <form
+        onSubmit={(e) => {
+          submithandler(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
