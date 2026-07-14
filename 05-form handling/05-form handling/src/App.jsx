0db082a835f1b5e6b14d8c4fcef92cf67f9fff44
tanishp1 <pawartanish73@gmail.com>
2026-07-14 22:07:08 +0530

    form handle

--- 05-form handling/05-form handling/src/App.jsx
+++ 05-form handling/05-form handling/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+
+  const submithandler = (e) => {
+    e.preventDefault()
+    console.log('form submit')
+  }
   return (
     <div>
-      
+      <form onSubmit={(e) => {
+        submithandler(e)
+      }}>
+        <input type="text" placeholder='Enter your name' />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
